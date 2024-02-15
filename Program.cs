c21adac8cc8519bf5a745063be6abc8cdf37ba5b
Radhika <radhikasn06@gmail.com>
2024-02-15 20:25:57 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WelComeApp;
+
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Welcome to Authenticate app");
+SignIn obj = new SignIn();
+Console.WriteLine(obj.Authenticate("admin", "admin@123"));
